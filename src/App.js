128741d67fee5a8c288ca0660d185feec0e8d60d
Gaurav-Vanarase <102522241+Gaurav-Vanarase@users.noreply.github.com>
2023-09-21 11:06:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Layout/Header";
+import { Fragment } from "react";
+function App() {
+  return (
+    <Fragment>
+      {/* <h2>Let's get started!</h2>
+       */}
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
